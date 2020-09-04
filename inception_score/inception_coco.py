cb6d36bd060992261b0145e6abb5f33ebc5a4587
lijiguo <lijiguo@bytedance.com>
2020-09-04 14:30:12 +0800

    fix bugs: (1) load data out of memory (2) does not support batch forward refer to (https://github.com/openai/improved-gan/pull/40/commits/0a09faccb45088695228bbf50435ee71e94eb2ce)

--- inception_score/inception_coco.py
+++ inception_score/inception_coco.py
@@ -1,13 +1,17 @@
-from model import get_inception_score
+from model import get_inception_features
 import os, tqdm
 from PIL import Image
 import numpy as np 
 import scipy
 import argparse
+import logging
 
 def get_parser():
     parser = argparse.ArgumentParser("inception score for coco dataset")
     parser.add_argument("--folder", type=str, default="/media/ubuntu/Elements1/dataset/COCO2014/val2014_j2k_QF50", help="")
+    parser.add_argument("--log_file", type=str, default="./inception_coco.log", help="")
+    parser.add_argument("--max_data_len", type=int, default=2048, help="")
+    parser.add_argument("--splits", type=int, default=10, help="")
     args = parser.parse_args()
     return args 
 
@@ -25,9 +29,11 @@ def preprocess(img):
     return img 
 
 
-def load_data(fullpath):
+
+def load_data(fullpath, max_data_len=2048):
     print("load data from: {}".format(fullpath))
     images = []
+    total_data_len = 0
     for path, subdirs, files in os.walk(fullpath):
         for name in tqdm.tqdm(files):
             if os.path.splitext(name)[-1] in ['.jpg', '.jpeg', '.png', '.j2k']:
@@ -41,12 +47,46 @@ def load_data(fullpath):
                     img = preprocess(img)
                     images.append(img)
                     img_fp.close()
-    print('images', len(images), images[0].shape)
+            if len(images)>=max_data_len:
+                total_data_len += len(images)
+                yield images
+                images = []
+    print('images number: ', total_data_len)
     return images
 
+def get_score(preds, splits=10):
+    scores = []
+    for i in range(splits):
+      part = preds[(i * preds.shape[0] // splits):((i + 1) * preds.shape[0] // splits), :]
+      kl = part * (np.log(part) - np.log(np.expand_dims(np.mean(part, 0), 0)))
+      kl = np.mean(np.sum(kl, 1))
+      scores.append(np.exp(kl))
+    return np.mean(scores), np.std(scores)
+
+def main(args):
+    logger = get_logger(args.log_file)
+    preds = []
+    for images in load_data(args.folder, max_data_len=args.max_data_len):
+        if len(images)>0:
+            preds_this = get_inception_features(images)
+            preds.append(preds_this)
+    preds = np.concatenate(preds)
+    score, std = get_score(preds, splits=args.splits)
+    logger.info("for folder: {}\nIS: ({:6.3f}, {:6.3f})".format(args.folder, score, std))
+
+
+def get_logger(log_file):
+    logFormatter = logging.Formatter("%(message)s")
+    rootLogger = logging.getLogger()
+    rootLogger.setLevel(logging.INFO)
+    fileHandler = logging.FileHandler(log_file)
+    fileHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(fileHandler)
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    rootLogger.addHandler(consoleHandler)
+    return rootLogger
 
 if __name__=="__main__":
     args = get_parser()
-    images = load_data(args.folder)
-    mean, std = get_inception_score(images)
-    print("IS mean: {:5.2f}, std: {:5.2f}".format(mean, std))
+    main(args)
